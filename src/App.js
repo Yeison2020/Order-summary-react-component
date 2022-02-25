613eddb34b58af1b1d03face004f6d986642ec6f
Yeison <enmanuelquiterios@gmail.com>
2022-02-24 23:45:43 -0500

    Adding elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import ScreenMain from "./components/ScreenMain";
+import img_main from "./assets/pattern-background-desktop.svg";
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="main__color">
+      <ScreenMain />
+    </div>
+  );
 };
 
 export default App;
